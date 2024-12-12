374f1b3bcef9d2ffc34b3c7f45d2b589bc441041
Marco Gulino <marco.gulino@gmail.com>
2024-12-12 09:01:21 +0000

    More improvements to INDI parsing

--- src/protocols/indi/indiparser.cpp
+++ src/protocols/indi/indiparser.cpp
@@ -1,6 +1,6 @@
 #include "indiparser.h"
 #include <set>
-#include <pugixml.hpp>
+#include <tinyxml2.h>
 
 #define LOGSCOPE "[INDIParser] "
 #ifdef ARDUINO
@@ -42,31 +42,33 @@ bool INDIParser::parseDevices(const char *xml, std::size_t len, INDIDevice::List
     buffer.clear();
     std::move(xml, xml+len, std::back_inserter(working_buffer));
 
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_buffer(working_buffer.data(), working_buffer.size(), pugi::parse_default | pugi::parse_fragment);
-    if(!result) {
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLError result = doc.Parse(working_buffer.data(), working_buffer.size());
+
+    if(result != tinyxml2::XML_SUCCESS) {
         std::move(working_buffer.begin(), working_buffer.end(), std::back_inserter(buffer));
         Log.infoln("Remaining buffer: %d chars, `%s`", buffer.size(), buffer.data());
         return false;
     }
-    Log.infoln("Buffer parse result: %d, desc=%s, offset=%d", static_cast<bool>(result), result.description(), result.offset);
-    for(const pugi::xml_node &element: doc.children()) {
+    tinyxml2::XMLElement* element = doc.FirstChildElement();
+    while(element) {
         // Log.infoln("Received element: %s, %s", element.name(), element.attribute("name"));
-        if(strcmp("DRIVER_INFO", element.attribute("name").as_string()) == 0) {
-            const char *deviceName = element.attribute("device").as_string();
+        if(strcmp("DRIVER_INFO", element->Attribute("name")) == 0) {
+            const char *deviceName = element->Attribute("device");
             Log.traceln(LOGSCOPE "Got DRIVER_INFO element for device %s", deviceName);
             uint32_t interfaces = 0;
-            pugi::xml_node child = element.find_child([](const pugi::xml_node &n){
-                return strcmp("defText", n.name()) == 0 &&
-                    strcmp("DRIVER_INTERFACE", n.attribute("name").as_string()) == 0;
-            
-            });
-            if(child) {
-                interfaces = child.text().as_int();
+            tinyxml2::XMLElement* child = element->FirstChildElement("defText");
+            while(child) {
+                if(strcmp(child->Attribute("name"), "DRIVER_INTERFACE") == 0) {
+                    interfaces = StringToNumber(child->GetText());
+                    break;
+                }
+                child = child->NextSiblingElement("defText");
             }
             inserter = INDIDevice{deviceName, interfaces};
         }
+        element = element->NextSiblingElement();
     }
     
-    return result;
+    return true;
 }
